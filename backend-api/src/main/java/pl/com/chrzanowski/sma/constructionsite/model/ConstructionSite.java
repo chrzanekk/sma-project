6c83239469ece09770dc2ab0cc5835b8f93e63fa
Konrad <chrzanekk@gmail.com>
2025-04-09 15:41:59 +0200

    init

---
+++ backend-api/src/main/java/pl/com/chrzanowski/sma/constructionsite/model/ConstructionSite.java
@@ -0,0 +1,47 @@
+package pl.com.chrzanowski.sma.constructionsite.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import pl.com.chrzanowski.sma.common.audit.AuditableEntity;
+import pl.com.chrzanowski.sma.common.enumeration.Country;
+import pl.com.chrzanowski.sma.company.model.Company;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "construction_site")
+public class ConstructionSite extends AuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "construction_site_seq")
+    @SequenceGenerator(name = "constr_site_seq", sequenceName = "construction_site_sequence", allocationSize = 1)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "name", length = 200)
+    private String name;
+
+    @Column(name = "address", length = 300)
+    private String address;
+
+    @Column(name = "country")
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    @Column(name = "short_name", length = 50)
+    private String shortName;
+
+    @Column(name = "code", length = 3)
+    private String code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    //todo make entity for ConstructionsiteContractor for many-to-many relation
+}
