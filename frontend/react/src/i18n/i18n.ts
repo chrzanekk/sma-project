2ba83ce1e7853c284b4bf0bca0d2747312cbce58
Konrad <chrzanekk@gmail.com>
2025-11-04 18:44:19 +0100

    frontend: employee service, filter, types and translations

--- frontend/react/src/i18n/i18n.ts
+++ frontend/react/src/i18n/i18n.ts
@@ -31,6 +31,8 @@ import positionsPl from './pl/positions.json'
 import positionsEn from './en/positions.json'
 import resourcesPl from './pl/resources.json'
 import resourcesEn from './en/resources.json'
+import employeePl from './pl/employee.json'
+import employeeEn from './en/employee.json'
 
 i18n
     .use(initReactI18next)
@@ -52,7 +54,8 @@ i18n
                 constructionSites: constructionSitesEn,
                 contracts: contractsEn,
                 positions: positionsEn,
-                resources: resourcesEn
+                resources: resourcesEn,
+                employee: employeeEn
             },
             pl: {
                 common: commonPl,
@@ -69,14 +72,15 @@ i18n
                 constructionSites: constructionSitesPl,
                 contracts: contractsPl,
                 positions: positionsPl,
-                resources: resourcesPl
+                resources: resourcesPl,
+                employee: employeePl
             },
         },
         lng: 'pl',
         fallbackLng: 'en',
         ns: ['common', 'navbar', 'userMenu', 'auth', 'footer', 'adminPanelMenu',
             'contractors', 'countries', 'contacts', 'companies', 'constructionSites',
-            'contracts', 'positions', 'resources'],
+            'contracts', 'positions', 'resources','employee'],
         defaultNS: 'common',
         interpolation: {
             escapeValue: false,
---
+++ frontend/react/src/services/employee-service.ts
@@ -0,0 +1,71 @@
+import {EmployeeFilter} from "@/types/filters/employee-filter.ts";
+import {serializeQueryParams} from "@/utils/query-params-serializer.ts";
+import {getSelectedCompany, getSelectedCompanyId} from "@/utils/company-utils.ts";
+import {api, getAuthConfig} from "@/services/axios-config.ts";
+import {parsePaginationResponse} from "@/utils/api-utils.ts";
+import {EmployeeDTO, FetchableEmployeeDTO} from "@/types/employee-types.ts";
+import {CompanyBaseDTO} from "@/types/company-types.ts";
+
+const EMPLOYEE_API_BASE = "/api/employees";
+
+export const getEmployeeByFilter = async (filter: EmployeeFilter) => {
+    try {
+        const queryParams = serializeQueryParams({
+            ...filter,
+            companyId: getSelectedCompanyId(),
+            size: filter.size || 10,
+            page: filter.page || 0
+        });
+        const response = await api.get(`${EMPLOYEE_API_BASE}?${queryParams}`, getAuthConfig())
+        const {items, totalPages} = parsePaginationResponse(response);
+        return {
+            employees: items as FetchableEmployeeDTO[],
+            totalPages
+        };
+    } catch (error) {
+        return {employees: [], totalPages: 1}
+    }
+}
+
+
+export const getEmployeeById = async (id: number) => {
+    try {
+        const response = await api.get(`${EMPLOYEE_API_BASE}/${id}`, getAuthConfig())
+        const positionDTO: FetchableEmployeeDTO = response.data;
+        return positionDTO;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const addEmployee = async (addEmployee: EmployeeDTO) => {
+    try {
+        const company: CompanyBaseDTO | null = getSelectedCompany();
+        const payload = {
+            ...addEmployee,
+            company
+        }
+        const response = await api.post(`${EMPLOYEE_API_BASE}`, payload, getAuthConfig());
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateEmployee = async (updateEmployee: EmployeeDTO) => {
+    try {
+        const company: CompanyBaseDTO | null = getSelectedCompany();
+        const payload = {
+            ...updateEmployee,
+            company
+        }
+        const response = await api.put(`${EMPLOYEE_API_BASE}/${updateEmployee.id}`, payload, getAuthConfig());
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deletePositonById = async (id: number) => {
+    await api.delete(`${EMPLOYEE_API_BASE}/${id}`, getAuthConfig())
+}
